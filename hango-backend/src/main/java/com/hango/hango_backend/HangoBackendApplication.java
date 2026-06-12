d11a7dbe9ca91262941096c931ad0111d21a5c1d
thanhthaoqn <thanhthao2004qn@gmail.com>
2026-06-12 15:55:13 +0700

    Create project

---
+++ hango-backend/src/main/java/com/hango/hango_backend/HangoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hango.hango_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangoBackendApplication.class, args);
+	}
+
+}
