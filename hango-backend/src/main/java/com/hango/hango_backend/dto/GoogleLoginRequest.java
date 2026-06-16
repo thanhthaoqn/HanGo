9f000f135bc35fc0374eec1ae841e77d1d77824f
thanhthaoqn <thanhthao2004qn@gmail.com>
2026-06-17 01:15:52 +0700

    add google login feature

--- hango-backend/src/main/java/com/hango/hango_backend/dto/GoogleLoginRequest.java
+++ hango-backend/src/main/java/com/hango/hango_backend/dto/GoogleLoginRequest.java
@@ -1,18 +1,10 @@
 package com.hango.hango_backend.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class GoogleLoginRequest {
     @NotBlank
-    @Email
-    private String email;
-
-    @NotBlank
-    private String fullName;
-
-    private String avatarUrl;
-    private String googleId;
+    private String idToken;
 }
