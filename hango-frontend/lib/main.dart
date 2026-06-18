1120b936dc38fb8763ed9de88b566d1a7a4f6be2
thanhthaoqn <thanhthao2004qn@gmail.com>
2026-06-18 19:24:31 +0700

    fix account detail page

--- hango-frontend/lib/main.dart
+++ hango-frontend/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'data/services/auth_service.dart';
-import 'presentation/pages/login_page.dart';
 import 'presentation/pages/learner/learner_home_page.dart';
-import 'presentation/pages/admin/admin_dashboard_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
